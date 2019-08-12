8d13190937f4d56fd48f252df4a3f1fb525dfb13
Zoran Igic <zoranigic@gmail.com>
2019-08-12 17:31:29 +0200

    v 1.0 Initial commit

---
+++ app/src/main/java/com/bestappsfree/scalebuttonexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.bestappsfree.scalebuttonexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button scaleButton = findViewById(R.id.testScaleButton);
+        scaleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Click!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
---
+++ scalebutton/src/main/java/com/bestappsfree/scalebutton/ScaleButton.java
@@ -0,0 +1,92 @@
+package com.bestappsfree.scalebutton;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ScaleButton extends AppCompatButton {
+    private List<Animation> mAnimationBuffer = new ArrayList<>();
+    private boolean mIsAnimating;
+
+    public ScaleButton(Context context)
+    {
+        super(context);
+
+    }
+
+    public ScaleButton(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+    }
+
+    public ScaleButton(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+
+    }
+
+
+//    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        if (event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            generateAnimation(1, 0.8f);
+            triggerNextAnimation();
+        }
+        else if (event.getAction() == MotionEvent.ACTION_CANCEL || event.getAction() == MotionEvent.ACTION_UP)
+        {
+            generateAnimation(0.8f, 1);
+            triggerNextAnimation();
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    private void generateAnimation(float from, float to)
+    {
+        ScaleAnimation scaleAnimation = new ScaleAnimation(from, to, from, to, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setDuration(100);
+        scaleAnimation.setAnimationListener(new ScaleAnimation.AnimationListener()
+        {
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                mIsAnimating = false;
+                triggerNextAnimation();
+            }
+        });
+
+        mAnimationBuffer.add(scaleAnimation);
+    }
+
+    private void triggerNextAnimation()
+    {
+        if (mAnimationBuffer.size() > 0 && !mIsAnimating)
+        {
+            mIsAnimating = true;
+            Animation currentAnimation = mAnimationBuffer.get(0);
+            mAnimationBuffer.remove(0);
+
+            startAnimation(currentAnimation);
+        }
+    }
+
+
+
+}
