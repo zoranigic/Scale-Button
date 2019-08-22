e2899466f5b5032a1269079f4ca63f1cae6d80e0
Zoran Igic <zoranigic@gmail.com>
2019-08-22 14:10:18 +0200

    Added scale Image button library

--- app/src/main/java/com/bestappsfree/scalebuttonexample/MainActivity.java
+++ app/src/main/java/com/bestappsfree/scalebuttonexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button scaleButton = findViewById(R.id.testScaleButton);
         scaleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Click!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ImageButton scaleImageButton = findViewById(R.id.testScaleImageButton);
+        scaleImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Click!", Toast.LENGTH_LONG).show();
